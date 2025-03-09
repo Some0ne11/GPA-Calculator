1ce5fcbec920b2bd9bd272594f9e5845c4a95345
Amrdanial_ <ammardanial274@gmail.com>
2025-03-09 15:43:42 +0800

    add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './components/GPACalculator.css';
+import GPACalculator from './components/GPACalculator';
+
+function App() {
+    return (
+        <div>
+            <GPACalculator />
+        </div>
+    );
+}
+
+export default App;
